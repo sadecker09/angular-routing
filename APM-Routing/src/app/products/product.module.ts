4912910dd06545e7288a8eee16c2e183ec347588
Sara Decker <sdecker@west-inc.com>
2021-04-15 15:34:08 -0600

    group product routes under a single componentless parent route

--- APM-Routing/src/app/products/product.module.ts
+++ APM-Routing/src/app/products/product.module.ts
@@ -15,31 +15,36 @@ import { RouterModule } from '@angular/router';
     // use forChild because defining feature routes (root routes defined in app.module)
     RouterModule.forChild([
       {
-        path: 'products',
-        component: ProductListComponent,
-      },
-      {
-        path: 'products/:id',
-        component: ProductDetailComponent,
-        resolve: { resolvedData: ProductResolver },
-      },
-      {
-        path: 'products/:id/edit',
-        component: ProductEditComponent,
-        resolve: { resolvedData: ProductResolver },
+        path: 'products', //componentless route
         children: [
           {
             path: '',
-            redirectTo: 'info',
-            pathMatch: 'full',
+            component: ProductListComponent,
           },
           {
-            path: 'info',
-            component: ProductEditInfoComponent,
+            path: ':id',
+            component: ProductDetailComponent,
+            resolve: { resolvedData: ProductResolver },
           },
           {
-            path: 'tags',
-            component: ProductEditTagsComponent,
+            path: ':id/edit',
+            component: ProductEditComponent,
+            resolve: { resolvedData: ProductResolver },
+            children: [
+              {
+                path: '',
+                redirectTo: 'info',
+                pathMatch: 'full',
+              },
+              {
+                path: 'info',
+                component: ProductEditInfoComponent,
+              },
+              {
+                path: 'tags',
+                component: ProductEditTagsComponent,
+              },
+            ],
           },
         ],
       },
