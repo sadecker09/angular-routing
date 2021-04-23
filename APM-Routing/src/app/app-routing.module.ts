cfff6cb8b10ae8296a06051c4605b728c1c964cc
Sara Decker <sdecker@west-inc.com>
2021-04-22 20:56:13 -0600

    demo preloading product module; requires switching canLoad guard back to canActivate guard since canLoad guard blocks preloading

--- APM-Routing/src/app/app-routing.module.ts
+++ APM-Routing/src/app/app-routing.module.ts
@@ -1,33 +1,36 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { PreloadAllModules, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 import { AuthGuard } from './user/auth.guard';
 
 @NgModule({
   imports: [
-    RouterModule.forRoot([
-      {
-        path: 'welcome',
-        component: WelcomeComponent,
-      },
-      // this is the default route (when app first loads)
-      {
-        path: '',
-        redirectTo: 'welcome',
-        pathMatch: 'full',
-      },
-      {
-        path: 'products',
-        canLoad: [AuthGuard],
-        loadChildren: () =>
-          import('./products/product.module').then((m) => m.ProductModule),
-      },
-      {
-        path: '**',
-        component: PageNotFoundComponent,
-      },
-    ]),
+    RouterModule.forRoot(
+      [
+        {
+          path: 'welcome',
+          component: WelcomeComponent,
+        },
+        // this is the default route (when app first loads)
+        {
+          path: '',
+          redirectTo: 'welcome',
+          pathMatch: 'full',
+        },
+        {
+          path: 'products',
+          canActivate: [AuthGuard],
+          loadChildren: () =>
+            import('./products/product.module').then((m) => m.ProductModule),
+        },
+        {
+          path: '**',
+          component: PageNotFoundComponent,
+        },
+      ],
+      { preloadingStrategy: PreloadAllModules }
+    ),
   ],
   exports: [RouterModule],
 })
